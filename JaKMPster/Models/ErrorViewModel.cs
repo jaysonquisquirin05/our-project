9bc5d9e7aa29b5b3780370594f77e3a52db484c8
Jayson Quisquirin <quisquirin.270031@baguio.sti.edu.ph>
2024-09-04 13:46:15 +0800

    Project

---
+++ JaKMPster/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JaKMPster.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
